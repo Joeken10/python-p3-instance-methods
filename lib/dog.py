b104dcf973eaa733c1b4365e2cbd8509a59047d3
Joeken10 <joekentafir@gmail.com>
2024-08-29 20:46:14 +0300

    add files

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
+  def bark(self):
+    print("Woof!")
+
+fido = Dog()
+fido.bark()
     # Class body goes here
 
     #Instance method definition
-    pass
+pass
+ 
